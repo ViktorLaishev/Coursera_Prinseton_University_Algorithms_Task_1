1bb3b3e83e46f68a228b2596eb21b351512864f8
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-01 12:15:31 +0200

    Update Start.java

--- Start.java
+++ Start.java
@@ -6,6 +6,6 @@ import edu.princeton.cs.algs4.StdIn;
 public class Start {
     public static void main (String [] args) {
         int n = StdIn.readInt();
-        System.out.println(n);
+        System.out.println(n); 
     }
 }
