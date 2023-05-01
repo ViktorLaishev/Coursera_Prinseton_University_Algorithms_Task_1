4e3754f07dbd552360fc378b21b5430a98a8a877
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-01 12:15:35 +0200

    Update Start.java

--- Start.java
+++ Start.java
@@ -3,7 +3,7 @@ import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 import edu.princeton.cs.algs4.StdIn;
 
-public class Start {
+public class Start { 
     public static void main (String [] args) {
         int n = StdIn.readInt();
         System.out.println(n); 
