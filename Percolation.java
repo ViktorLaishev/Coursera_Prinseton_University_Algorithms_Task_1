595c14ae1e2ec3ae4da7ecd7b641b78cf6e388b9
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-11 18:44:05 +0200

    Update Percolation.java

--- Percolation.java
+++ Percolation.java
@@ -2,7 +2,7 @@ import edu.princeton.cs.algs4.StdRandom;
 import edu.princeton.cs.algs4.StdStats;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
-public class Percolation {
+public class Percolation { 
  
     private boolean [] sites;  
     private WeightedQuickUnionUF uf; 
