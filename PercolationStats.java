7f12c82b83224ecfe9b4f2190a22377f2b86f7c5
Viktor Laishev <130911518+ViktorLaishev@users.noreply.github.com>
2023-05-13 16:47:07 +0200

    Update PercolationStats.java

--- PercolationStats.java
+++ PercolationStats.java
@@ -1,5 +1,5 @@
 import edu.princeton.cs.algs4.StdRandom;
-import edu.princeton.cs.algs4.StdStats;  
+import edu.princeton.cs.algs4.StdStats;   
 import edu.princeton.cs.algs4.StdIn;
 import edu.princeton.cs.algs4.StdOut;
 
